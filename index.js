ebe7c73fa2ec226207f58e471d49f16494ac2d74
System Administrator <leued2015@outlook.com>
2018-04-20 11:38:02 +0800

    调通

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-ddddddd
+#!/usr/bin/env node
+
+var path = require('path');
+var download = require('download-git-repo')
+var program = require('commander')
+
+// var arguments = process.argv.splice(2);
+
+program
+    .version(require('./package').version)
+    .option('-i, --init', '初始化项目文件夹')
+    .parse(process.argv);
+
+/** init */
+if (program.init) {
+    console.log('下载中')
+    download('leued/webpack-tpl', './test', { clone: true }, function(err) {
+        console.log('下载完成')
+    })
+}
